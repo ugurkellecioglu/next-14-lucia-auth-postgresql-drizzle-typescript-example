38c54b3c9dac39b378075e180d707701bc59cdd0
Ugur <ugur.kellecioglu@outlook.com>
2024-02-21 15:10:42 +0300

    Update database import in lucia/adapter.ts

--- lib/lucia/adapter.ts
+++ lib/lucia/adapter.ts
@@ -1,5 +1,5 @@
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
-import db from "../database/db"
+import db from "../database"
 
 import * as schema from "../database/schema"
 
