1a996772996d0de674551b84251482abdd410eed
Ugur <ugur.kellecioglu@outlook.com>
2024-03-09 14:12:46 +0300

    Add GitHub OAuth support

--- lib/lucia/oauth.ts
+++ lib/lucia/oauth.ts
@@ -1,7 +1,15 @@
-import { Google, generateCodeVerifier, generateState } from "arctic"
+import { GitHub, Google } from "arctic"
 
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
   process.env.NEXT_PUBLIC_BASE_URL + "/api/oauth/google"
+)
+
+export const github = new GitHub(
+  process.env.GITHUB_CLIENT_ID!,
+  process.env.GITHUB_CLIENT_SECRET!,
+  {
+    redirectURI: process.env.NEXT_PUBLIC_BASE_URL + "/api/oauth/github",
+  }
 )
